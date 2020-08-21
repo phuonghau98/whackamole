94948176d05599af2e7ec7e9178b72b32ae2dee0
Phuong Hau <hvphuong98@gmail.com>
2020-08-21 09:35:23 +0700

    init

---
+++ build/precache-manifest.22c4e7c22487d57a21353442ff723f91.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3611e1f0788b3f3eb4ac1a3c2c8d374",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d065777768655062f64",
+    "url": "/static/css/2.04ff9426.chunk.css"
+  },
+  {
+    "revision": "a85a1cece1a2e738be13",
+    "url": "/static/css/main.eb6714d4.chunk.css"
+  },
+  {
+    "revision": "0d065777768655062f64",
+    "url": "/static/js/2.4d83eaa8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.4d83eaa8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a85a1cece1a2e738be13",
+    "url": "/static/js/main.f2d369db.chunk.js"
+  },
+  {
+    "revision": "723f06820a790b9e01b9",
+    "url": "/static/js/runtime-main.f14e31eb.js"
+  },
+  {
+    "revision": "44ceb7e0bc50351539d10b402e30d755",
+    "url": "/static/media/congratulations.44ceb7e0.mp3"
+  },
+  {
+    "revision": "938a0913e59c818186f3663e2aa9eeb5",
+    "url": "/static/media/dirt.938a0913.svg"
+  },
+  {
+    "revision": "53b9feb95b9b6138c0dcd0ae0e8e4477",
+    "url": "/static/media/firework.53b9feb9.gif"
+  },
+  {
+    "revision": "3a50daeaeb202a08120e20e40037b0e5",
+    "url": "/static/media/gameplay.3a50daea.mp3"
+  },
+  {
+    "revision": "33f3e860a3b9964534797a71e3285347",
+    "url": "/static/media/mole.33f3e860.svg"
+  }
+]);
